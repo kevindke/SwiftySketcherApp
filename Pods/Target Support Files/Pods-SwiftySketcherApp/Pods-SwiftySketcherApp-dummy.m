5e7f424d4673419c43b3391b7ebde01cb3b959cc
Shijir Tsogoo <shijir99@gmail.com>
2016-05-30 19:12:39 -0700

    retrieving data from Firebase

---
+++ Pods/Target Support Files/Pods-SwiftySketcherApp/Pods-SwiftySketcherApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftySketcherApp : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftySketcherApp
+@end
--- SwiftySketcherApp/ViewController.swift
+++ SwiftySketcherApp/ViewController.swift
@@ -5,15 +5,32 @@
 //  Created by Shijir Tsogoo on 5/30/16.
 //  Copyright © 2016 Shijir Tsogoo. All rights reserved.
 //
-
+import Firebase
 import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let ref = FIRDatabase.database().reference()
+
+        ref.child("Session").observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
+ 
+            let session = snapshot.value as! String
+            print(session)
+            
+            
+            
+        })
+        
+
+   
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
