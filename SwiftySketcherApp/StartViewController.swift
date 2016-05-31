fd4014d467f39eb557102f2bcae402e4bcec0bf1
Shijir Tsogoo <shijir99@gmail.com>
2016-05-31 00:37:51 -0700

    add in sessions

--- SwiftySketcherApp/StartViewController.swift
+++ SwiftySketcherApp/StartViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import Firebase
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var sessions = []
+    let sessionsTableIdentifier = "sessionsTableIdentifier"
+    var tableViewController = UITableViewController(style: .Plain)
 
+    @IBOutlet var sessionLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        let ref = FIRDatabase.database().reference()
+        
+        ref.child("Sessions").observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
+            
+            
+            
+            if snapshot.exists()  {
+            
+                let fbSessions = snapshot.value as! [String: AnyObject]
+                self.sessions = Array(fbSessions.values)
+                
+                if self.sessions.count==1 {
+                    
+                    self.sessionLabel.text = "There is a session."
+                    
+                }
+                
+                if self.sessions.count>1{
+                    
+                    self.sessionLabel.text = "There are \(self.sessions.count) sessions."
+                    
+                }
+                
+                self.tableView.reloadData()
+                
+            }else{
+                
+                self.sessionLabel.text = "No sessions. Create your session."
+                self.sessions = []
+                self.tableView.reloadData()
+                
+            }
+
+            
+            
+        })
 
         // Do any additional setup after loading the view.
+    
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sessions.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(sessionsTableIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(
+                style: UITableViewCellStyle.Default,
+                reuseIdentifier: sessionsTableIdentifier)
+        }
+        cell?.textLabel?.text = self.sessions[indexPath.row] as! String
+        return cell!
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
